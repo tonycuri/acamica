27d3f68b8a7759adac6819eda3f8c742b39f33fe
tonycuri <curicaballero@gmail.com>
2018-09-15 17:21:28 -0300

    agregamos el mapa al proyecto

--- recursos/08/Recursos Mapa Interactivo/js/apis.js
+++ recursos/08/Recursos Mapa Interactivo/js/apis.js
@@ -1,13 +1,14 @@
 var mapa // Mapa que vamos a modificar
 
 /* Crear la variable posicionCentral con las coordenadas donde se va a centrar el mapa */
+var posicionCentral = {lat:-34.693391, lng: -58.458985}
 
 // Inicializa el mapa con un valor de zoom y una locación en el medio
 function inicializarMapa () {
     /* Modificá la variable mapa con el constructor Map().
     Tendrás que asignarle un valor de zoom y
     un centro igual a la variable posicionCentral. */
-
+  mapa = new google.maps.Map(document.getElementById("map"), {zoom: 15, center: posicionCentral})
   geocodificadorModulo.inicializar()
   marcadorModulo.inicializar()
   direccionesModulo.inicializar()
