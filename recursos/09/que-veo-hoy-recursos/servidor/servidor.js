8b5688a1c993ef184c2c21f18db1e3de6255a56b
tonycuri <curicaballero@gmail.com>
2018-09-27 20:49:28 -0300

    iniciamos el proyecto que veo hoy

---
+++ recursos/09/que-veo-hoy-recursos/servidor/servidor.js
@@ -0,0 +1,25 @@
+//paquetes necesarios para el proyecto
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+var app = express();
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
+//seteamos el puerto en el cual va a escuchar los pedidos la aplicación
+var puerto = '8080';
+
+app.listen(puerto, function () {
+  console.log( "Escuchando en el puerto " + puerto );
+});
+
+
+//puerto 3306
+//pass admin123
